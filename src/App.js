3baf6af8821c3ea691441cca96816ef9680b6f0d
Allen Kou <imakou@gmail.com>
2020-02-11 20:26:30 -0500

    wording

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { makeStyles, Button } from "@material-ui/core";
+import {
+  Dialog,
+  Button,
+  makeStyles,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+} from "@material-ui/core";
 import "./index.css";
 import { initializeApp } from "firebase";
 import "firebase/app";
@@ -18,31 +25,32 @@ const useStyles = makeStyles(theme => ({
   button: {
     fontSize: 24,
     fontWeight: "bold",
-    width:'100%',
-    margin:'15px 0',
+    width: "100%",
+    margin: "15px 0"
   },
   container: {
     fontSize: 20,
     color: "#fff",
     textShadow: "1px 2px 0px black;",
     padding: 10,
-    backgroundColor:'rgba(0,0,0,0.5)',
-    margin:'15px 0',
+    backgroundColor: "rgba(0,0,0,0.5)",
+    margin: "15px 0"
   },
   bottomConainter: {
     color: "#fff",
     textShadow: "1px 2px 0px black;",
     padding: 10,
-    backgroundColor:'rgba(30,144,255,0.5)'
+    backgroundColor: "rgba(30,144,255,0.5)"
   },
-  goalSpan:{
-    color: 'rgba(200, 52, 52, 1)',
-    fontSize: 15,
+  goalSpan: {
+    color: "rgba(230, 52, 52, 1)",
+    fontSize: 15
   }
 }));
 
 const App = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
   const [localCount, setCount] = useState("-");
   const rootRef = firebaseApp.database().ref("/");
   useEffect(() => {
@@ -51,9 +59,22 @@ const App = () => {
       setCount(counter);
     });
   }, []);
-  const updateRemoteCounter =()=>{
-    rootRef.child('counter').set(localCount+1)
-  }
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const completedDJ = () => {
+    rootRef.child("counter").set(localCount + 1);
+    setOpen(false);
+  };
+
+
+
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
@@ -68,11 +89,11 @@ const App = () => {
           <br />
           晚上8點恭誦彌勒救苦真經三遍
           <br />
-          參加者誦經前請點擊一次計數器連結
+          參加者誦經後請點擊一次計數器
           <br />
-          把愛串起來 凝聚您我慈悲之心 
+          把愛串起來 凝聚您我慈悲之心
           <br />
-          叩求諸天仙佛撥轉 感動上天
+          叩求諸天仙佛慈悲撥轉 感動上天
           <br />
           挽化疫情止息 天下眾生平安
           <br />
@@ -84,25 +105,47 @@ const App = () => {
           flexDirection="column"
           p={3}
         >
-          <Typography variant="h5">累計次數：{localCount}</Typography>
-          <span className={classes.goalSpan}>距離目標還有：{localCount - 3600} 次</span>
+          <Typography variant="h5">誦經累計次數：{localCount}</Typography>
+          <span className={classes.goalSpan}>
+            恭誦彌勒救苦真經：{localCount * 3} 遍
+          </span>
         </Box>
 
         <Box display="flex" justifyContent="center">
           <Button
             variant="contained"
             color="primary"
-            onClick={() => updateRemoteCounter()}
+            onClick={() => handleClickOpen()}
             className={classes.button}
           >
-          👉 我已經完成誦經 🙏
+            🙏 我已經完成誦經 🙏
           </Button>
         </Box>
-        <Typography variant="h6" className={classes.bottomConainter}>
-          迴向文：<br/>
-          願以此誦經功德迴向<br/>
-          平息新冠狀肺炎疫情 眾生平安
-        </Typography>
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+          迴向文：
+          </DialogTitle>
+          <DialogContent>
+            <Typography variant="h6">
+              願以此誦經功德迴向
+              <br />
+              平息新冠狀肺炎疫情
+              <br />
+              眾生平安
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={completedDJ} color="primary">
+              我願意將誦經功德迴向給受苦難的眾生
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Box>
     </Container>
   );
